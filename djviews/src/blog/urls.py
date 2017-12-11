e3418d2d38e5c49b0c8ba4183b9fd6379f3e43ea
travishen <travishen.tw@gmail.com>
2017-12-11 23:19:59 +0800

    create view

--- djviews/src/blog/urls.py
+++ djviews/src/blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import post_model_list_view, post_model_detail_view
+from .views import post_model_list_view, post_model_detail_view, post_model_create_view
 
 urlpatterns = [
     url(r'^$', post_model_list_view, name='list'),
-    url(r'^detail/(?P<id>\d+)/$', post_model_detail_view, name='detail')
+    url(r'^detail/(?P<id>\d+)/$', post_model_detail_view, name='detail'),
+    url(r'^create/$', post_model_create_view, name='create')
 ]
 
--- djviews/src/blog/views.py
+++ djviews/src/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import PostModel
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+
+from .forms import PostModelForm
 
 
 @login_required()
@@ -13,7 +16,7 @@ def some_view(request):
 def post_model_list_view(request):
     posts = PostModel.objects.all()
     # return HttpResponse(posts)
-    template = 'blog/hello_world.html'
+    template = 'blog/list-view.html'
     context = {
         "posts": posts
     }
@@ -31,4 +34,20 @@ def post_model_detail_view(request, id=None):
 
 
 def post_model_create_view(request):
-    pass
+    form = PostModelForm(request.POST or None)
+    context = {
+        'form': form
+    }
+
+    if form.is_valid():
+        f = form.save(commit=False)
+        # modify form.attributes here...
+        f.save()
+        messages.success(request, "Create a new post successfully!")
+        # return HttpResponseRedirect('/blog/detail/{id}'.format(id=f.id))
+        context = {
+            'form': PostModelForm()
+        }
+    template = 'blog/create-view.html'
+
+    return render(request, template, context)
