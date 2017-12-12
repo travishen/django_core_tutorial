62de7ebd21c47605a7f5557241cff4c6e0b57852
travishen <travishen.tw@gmail.com>
2017-12-12 18:55:56 +0800

    update djviews

--- djviews/src/blog/urls.py
+++ djviews/src/blog/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 
-from .views import post_model_list_view, post_model_detail_view, post_model_create_view
+from .views import post_model_list_view, post_model_detail_view, post_model_create_view, post_model_update_view
+from .views import post_model_view
 
 urlpatterns = [
-    url(r'^$', post_model_list_view, name='list'),
-    url(r'^detail/(?P<id>\d+)/$', post_model_detail_view, name='detail'),
-    url(r'^create/$', post_model_create_view, name='create')
+    # url(r'^$', post_model_list_view, name='list'),
+    # url(r'^detail/(?P<id>\d+)/$', post_model_detail_view, name='detail'),
+    # url(r'^create/$', post_model_create_view, name='create'),
+    # url(r'^update/(?P<id>\d+)/$', post_model_update_view, name='update'),
+    url(r'^$', post_model_view, name='list'),
+    url(r'^detail/(?P<id>\d+)/$', post_model_view, name='detail'),
+    url(r'^create/$', post_model_view, name='create'),
+    url(r'^update/(?P<id>\d+)/$', post_model_view, name='update'),
 ]
 
--- djviews/src/blog/views.py
+++ djviews/src/blog/views.py
@@ -4,7 +4,7 @@ from .models import PostModel
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q
 from .forms import PostModelForm
 
 
@@ -25,6 +25,10 @@ def post_model_list_view(request):
 
 def post_model_detail_view(request, id=None):
     post = get_object_or_404(PostModel, id=id)
+
+    if request.method == 'POST':
+        post.delete()
+        return HttpResponseRedirect('/blog/')
 
     template = 'blog/detail/detail.html'
     context = {
@@ -49,5 +53,80 @@ def post_model_create_view(request):
             'form': PostModelForm()
         }
     template = 'blog/create-view.html'
+
+    return render(request, template, context)
+
+
+def post_model_update_view(request, id=None):
+    post = get_object_or_404(PostModel, id=id)
+    form = PostModelForm(request.POST or None, instance=post)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        f = form.save(commit=False)
+        # modify form.attributes here...
+        f.save()
+        return HttpResponseRedirect('/blog/detail/{id}'.format(id=f.id))
+    template = 'blog/update-view.html'
+
+    return render(request, template, context)
+
+
+def post_model_view(request, id=None):
+
+    context = {}
+
+    if id is not None:
+        post = get_object_or_404(PostModel, id=id)
+    else:
+        post = None
+
+    # view detail
+    if 'detail' in request.get_full_path():
+
+        # delete
+        if request.method == 'POST':
+            post.delete()
+            return HttpResponseRedirect('/blog/')
+
+        template = 'blog/detail/detail.html'
+        context = {
+            'post': post
+        }
+
+    # create / update
+    if 'update' in request.get_full_path() or 'create' in request.get_full_path():
+        if id is None:
+            template = 'blog/create-view.html'
+        else:
+            template = 'blog/update-view.html'
+
+        form = PostModelForm(request.POST or None, instance=post)
+        context = {
+            'form': form
+        }
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            return HttpResponseRedirect('/blog/detail/{id}'.format(id=f.id))
+
+    # list
+    if not context:
+        template = 'blog/list-view.html'
+
+        posts = PostModel.objects.all()
+        queries = request.GET
+
+        if queries.get('title'):
+            title = queries.get('title')
+            posts = posts.filter(
+                Q(title__icontains=title) |
+                Q(content__icontains=title)
+            )
+
+        context = {
+            'posts': posts
+        }
 
     return render(request, template, context)
