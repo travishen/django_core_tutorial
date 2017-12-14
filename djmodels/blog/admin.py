1cf820f0c3dd9a0200543c38aa0230d8baadd9c7
ssivart <travishen.tw@gmail.com>
2017-12-14 13:38:41 +0800

    modify djmodels

--- djmodels/blog/admin.py
+++ djmodels/blog/admin.py
@@ -4,4 +4,27 @@ from django.contrib import admin
 
 from .models import PostModel
 
-admin.site.register(PostModel)
+
+class PostModelAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'publish_date',
+        'updated',
+        'timestamp',
+        'new_content'
+    ]
+    readonly_fields = [
+        'updated',
+        'timestamp',
+        'new_content'
+    ]
+
+    @staticmethod
+    def new_content(obj,  *args, **kwargs):
+        return str(obj.title) + str(obj.publish_date)
+
+    class Meta:
+        model = PostModel
+
+
+admin.site.register(PostModel, PostModelAdmin)
--- djmodels/blog/models.py
+++ djmodels/blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.db.models import (CharField, TextField,
                               IntegerField, DateField,
-                              SlugField)
+                              SlugField, DateTimeField)
 from django.utils.encoding import smart_text
 from .validators import validate_author_email
 from django.utils.text import slugify
+
+from django.db.models.signals import post_save, pre_save
 
 PUBLISH_CHOICES = (
     ('draft', 'Draft'),
@@ -14,7 +16,14 @@ PUBLISH_CHOICES = (
 
 
 class PostModel(models.Model):
-    title = CharField(max_length=20, default='', unique=True)
+    title = CharField(max_length=20,
+                      default='',
+                      unique=True,
+                      error_messages={
+                          'unique': 'this title is not unique, please try again!',
+                          'blank': 'this title is blank, please try again!'
+                      },
+                      help_text='must be an unique title')
     content = TextField(null=True, blank=True)
     publish = CharField(max_length=120, default='draft', choices=PUBLISH_CHOICES)
     view_count = IntegerField(default=0)
@@ -24,14 +33,16 @@ class PostModel(models.Model):
                              null=True,
                              blank=True)
     slug = SlugField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        # if not self.slug:
+        #     self.slug = slugify(self.title)
         super(PostModel, self).save(*args, **kwargs)
 
     def __unicode__(self):
@@ -39,5 +50,25 @@ class PostModel(models.Model):
 
     def __str__(self):
         return smart_text(self.title)
+
+
+def post_model_save_receiver(sender, instance, created, *args, **kwargs):
+    print('call post_model_save_receiver')
+    print(created)
+    if created:
+        if not instance.slug and instance.title:
+            instance.slug = slugify(instance.title)
+            instance.save()
+
+
+post_save.connect(post_model_save_receiver, sender=PostModel)
+
+
+def post_model_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug and instance.title:
+        instance.slug = slugify(instance.title)
+
+
+# pre_save.connect(post_model_pre_save_receiver, sender=PostModel)
 
 
